cd0d21fb18fdd385ab4b8de90274689fcc489fb4
shivamsnap <64766657+shivamsnap@users.noreply.github.com>
2023-12-01 19:38:13 +0530

    Add files via upload

---
+++ Day_11/Solution.java
@@ -0,0 +1,17 @@
+public static Node reverseDLL(Node  head)
+{
+    Node temp = head;
+    
+    while (temp.next!=null) {
+        Node t = temp.prev;
+        temp.prev = temp.next;
+        temp.next = t;
+        temp = temp.prev;
+    }
+    Node t = temp.prev;
+    temp.prev = temp.next;
+    temp.next = t;
+    head = temp;
+    
+    return head;
+}
