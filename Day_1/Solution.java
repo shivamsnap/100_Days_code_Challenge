55a235c12ed77148fc5e6abfa897e5e7bb4b7ca8
shivamsnap <64766657+shivamsnap@users.noreply.github.com>
2023-12-01 19:18:34 +0530

    Add files via upload

---
+++ Day_1/Solution.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    //Function to return list of integers visited in snake pattern in matrix.
+    static ArrayList<Integer> snakePattern(int matrix[][])
+    {
+        // code here 
+         ArrayList<Integer> snake = new ArrayList<>();
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    snake.add(matrix[i][j]);
+                }
+            } else {
+                for (int j = n - 1; j >= 0; j--) {
+                    snake.add(matrix[i][j]);
+                }
+            }
+        }
+
+        return snake;
+    }
+
+    
+}
