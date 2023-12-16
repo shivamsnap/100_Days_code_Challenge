5deaf468fe9f31bafb227a95851a18d7ed5005f8
Shivam Singh <64766657+shivamsnap@users.noreply.github.com>
2023-12-16 11:33:17 +0530

    Add files via upload

---
+++ Day_37/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public static int smallestSubWithSum(int a[], int n, int x) {
+        // Your code goes here 
+        int ans=0;
+        int sum=0;
+        int maxSum=0;
+        
+        int start=0;
+        
+        int minLength = n+1;
+        
+        for(int i=0;i<n;i++){
+            
+            sum+=a[i];
+            
+            if(sum>x){  //is sum is greater then X then subtract sum
+            
+            while(sum>x){
+                sum-=a[start++];
+            }
+            minLength = Math.min(minLength,i-start+2);
+            
+            } 
+        }
+        return minLength ==n+1?0:minLength;
+    }
+}
